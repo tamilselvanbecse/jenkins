751e5ca8f625b32d073f17901b368e7e24dbd9ec
tamilselvanbecse <136075212+tamilselvanbecse@users.noreply.github.com>
2023-06-24 11:07:49 +0530

    Update JenkinsExample.java

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -3,7 +3,7 @@ public class JenkinsExample {
  public static void main(String[] args) {
   // TODO Auto-generated method stub
     System.out.println("Hello Tamil");
-	System.out.println("Welcome to Jenkins");
+	System.out.println("Welcome to Jenkins World");
 
 
  }
